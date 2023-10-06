15aa8a5c2f9ebaa2e41081496b618095d5fdb015
Александр Дубровин <xah799@mail.ru>
2023-10-06 13:02:40 +0300

    Implemented ImageBinarization class with binarizeWithThresholdCustom() method

--- src/ImageBinarization.java
+++ src/ImageBinarization.java
@@ -1,14 +1,22 @@
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 
 public class ImageBinarization {
-    private static final double THRESH = 75; // Пороговое значение
-    private static final double MAX_BINARY_VALUE = 255; // Максимальное значение яркости пикселя
-    private static final int THRESHOLD_TYPE = 0; // Тип порога бинаризации
-    private final Mat dst = new Mat(); // Бинаризация изображения
+    private static final double THRESH = 64.0; // Пороговое значение
+    private static final double MAX_BINARY_VALUE = 255.0; // Максимальное значение яркости пикселя
+    private static final double MIN_BINARY_VALUE = 0.0; // Максимальное значение яркости пикселя
+    private Mat dst; // Матрица для бинарного изображения
 
-    public Mat binarizeWithThreshold(Mat grayImage) {
-        Imgproc.threshold(grayImage, dst, THRESH, MAX_BINARY_VALUE, THRESHOLD_TYPE);
+    // Метод пороговой бинаризации изображения
+    public Mat binarizeWithThresholdCustom(Mat grayImage) {
+        dst = new Mat(grayImage.size(), grayImage.type());
+        for (int y = 0; y < grayImage.rows(); y++) {
+            for (int x = 0; x < grayImage.cols(); x++) {
+                if (grayImage.get(y, x)[0] >= THRESH)
+                    dst.put(y, x, MAX_BINARY_VALUE);
+                else
+                    dst.put(y, x, MIN_BINARY_VALUE);
+            }
+        }
         return dst;
     }
 
