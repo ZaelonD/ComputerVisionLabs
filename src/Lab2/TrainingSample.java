5975b955d71b4914aa55e667d34184701feda275
Александр Дубровин <xah799@mail.ru>
2024-02-09 17:40:54 +0300

    Created TrainingSample class

---
+++ src/Lab2/TrainingSample.java
@@ -0,0 +1,16 @@
+package Lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainingSample {
+    private final List<Image> trainingSample;
+
+    public TrainingSample() {
+        this.trainingSample = new ArrayList<>();
+    }
+
+    public List<Image> getTrainingSample() {
+        return trainingSample;
+    }
+}
