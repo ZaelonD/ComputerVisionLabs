c850e6c9714ccd20836517830448a7b5bfb1e7d8
Александр Дубровин <xah799@mail.ru>
2024-02-09 17:42:18 +0300

    Created TestSample class

---
+++ src/Lab2/TestSample.java
@@ -0,0 +1,16 @@
+package Lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSample {
+    private final List<Image> testSample;
+
+    public TestSample() {
+        this.testSample = new ArrayList<>();
+    }
+
+    public List<Image> getTestSample() {
+        return testSample;
+    }
+}
